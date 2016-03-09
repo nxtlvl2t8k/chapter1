d03f96b0426ba36a18160a4bc62ddd19236eb7f3
Scott Mayhew <mayhews@hotmail.com>
2016-03-09 06:02:01 -0800

    More repairs

--- Contacts/Contacts/main.m
+++ Contacts/Contacts/main.m
@@ -17,19 +17,32 @@ int main(int argc, const char * argv[]) {
         char createNew[255];
         
         while (true) {
+            NSLog(@"What would you like do next? new - Create a new contact list - List all contacts quit - Exit Application > %s", createNew);
+            fgets(createNew, 255, stdin);
+            NSString *getMenuItem = [NSString stringWithUTF8String:createNew];
+            NSString *itemMinusReturn = [[getMenuItem componentsSeparatedByString:@"\n"] firstObject];
+            if ([itemMinusReturn isEqualToString: @"new"]) {
+
+                NSLog(@"Enter your Name and Email %@", newContact);
+                
+                InputCollector *newUserInfo = [[InputCollector alloc] inputForPrompt:newContact];
+                
+                Contact *newContact = [[Contact alloc] name];
+
+                ContactList *newEntry = [[ContactList alloc] addContact:newContact];
+                
+                
+                
+                
+                
+            }else if ([itemMinusReturn isEqualToString: @"quit"]) {
+                return 0;
+            }
             
-        
-        
-        NSLog(@"What would you like do next? new - Create a new contact list - List all contacts quit - Exit Application > %s", createNew);
-        fgets(createNew, 255, stdin);
-            if (@"new") {
-                
-                
-            }
-        
-        
-        
-            }
+            
+            
+            
+        }
     }
     return 0;
 }
